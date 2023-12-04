aa0b96807e41d944679f26a086079c05e48ba8cb
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 12:29:18 +0100

    remove defaut dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,6 @@ export const Navbar = () => {
             </li>
           </ul>
         </div>
-
         <a
           className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 rounded-xl transition duration-200"
           href="#"
