03e7a79363f70f6129da92d1252508e784f61628
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 11:09:50 +0100

    navbar but not responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex w-full justify-between p-4 bg-yellow-400 items-center">
+      <ul className="flex gap-4 items-center">
+        <li>
+          <a href="#" className="text-3xl font-bold mr-6">
+            COGIP
+          </a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Invoices</a>
+        </li>
+        <li>
+          <a href="#">Companies</a>
+        </li>
+        <li>
+          <a href="#">Contacts</a>
+        </li>
+      </ul>
+      <div className="flex gap-4 items-center">
+        <button>Sign Up</button>
+        <a href="#">Login</a>
+      </div>
+    </div>
+  );
 };
