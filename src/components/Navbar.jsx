d5dc66e32c867eec11022cc700094b21c5235447
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 12:05:58 +0100

    hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="flex w-full justify-between p-4 bg-yellow-400 items-center">
-      <ul className="flex gap-4 items-center">
-        <li>
-          <a href="#" className="text-3xl font-bold mr-6">
+    <div>
+      <nav className="relative justify-between px-6 py-4 flex items-center bg-yellow-400">
+        <div className="flex items-center justify-between w-full lg:w-auto">
+          <a className="text-3xl font-bold leading-none mr-8" href="#">
             COGIP
           </a>
-        </li>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Invoices</a>
-        </li>
-        <li>
-          <a href="#">Companies</a>
-        </li>
-        <li>
-          <a href="#">Contacts</a>
-        </li>
-      </ul>
-      <div className="flex gap-4 items-center">
-        <button>Sign Up</button>
-        <a href="#">Login</a>
+          <div className="lg:hidden">
+            <button
+              className="navbar-burger flex items-center text-black bg-transparent p-3"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="block h-4 w-4 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={toggleMenu}
+              >
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+            </button>
+          </div>
+          <ul className="hidden lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+            <li>
+              <a className="text-sm" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="text-sm" href="#">
+                Invoices
+              </a>
+            </li>
+            <li>
+              <a className="text-sm" href="#">
+                Companies
+              </a>
+            </li>
+            <li>
+              <a className="text-sm" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <a
+          className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 rounded-xl transition duration-200"
+          href="#"
+        >
+          Sign In
+        </a>
+        <a
+          className="hidden lg:inline-block py-2 px-6 text-sm transition duration-200"
+          href="#"
+        >
+          Sign up
+        </a>
+      </nav>
+      <div
+        className={`navbar-menu ${
+          isMenuOpen ? "block" : "hidden"
+        } navbar-menu relative z-50`}
+      >
+        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+          <div className="flex items-center mb-8">
+            <a className="mr-auto text-3xl font-bold leading-none" href="#">
+              COGIP
+            </a>
+            <button className="navbar-close" onClick={toggleMenu}>
+              <svg
+                className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-700"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={toggleMenu}
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div>
+            <ul>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-600 hover:bg-gray-100 hover:text-black rounded"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-600 hover:bg-gray-100 hover:text-black rounded"
+                  href="#"
+                >
+                  Invoices
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-600 hover:bg-gray-100 hover:text-black rounded"
+                  href="#"
+                >
+                  Companies
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-600 hover:bg-gray-100 hover:text-black rounded"
+                  href="#"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-auto">
+            <div className="pt-6">
+              <a
+                className="block mb-3 p-3 leading-loose text-xs text-center font-semibold leading-none hover:bg-gray-100 rounded-xl"
+                href="#"
+              >
+                Sign in
+              </a>
+              <a
+                className="block mb-3 p-2 leading-loose text-xs text-center font-semibold bg-yellow-400 hover:bg-yellow-200 rounded-xl"
+                href="#"
+              >
+                Login
+              </a>
+            </div>
+            <p className="my-4 text-xs text-center">
+              <span>by BibouTeam ❤️‍🔥</span>
+            </p>
+          </div>
+        </nav>
       </div>
     </div>
   );
