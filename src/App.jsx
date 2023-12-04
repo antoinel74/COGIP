34d8c57fc36f92d8abcec10a54e601b49cf32c2f
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 12:47:52 +0100

    add divider

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Home } from "./views/dashboard/home";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { Divider } from "./components/Divider";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Divider />
       <Home />
       <Footer />
     </>
---
+++ src/components/Divider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Divider = () => {
+  return (
+    <div className=" w-full bg-yellow-400 max-h-24">
+      <img src="/divider.png" className="px-8" />
+    </div>
+  );
+};
