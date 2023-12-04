f6660f2cbd94899069378d59ee93bd79ea2e6af9
Thomas Gomree <gomreethomas@gmail.com>
2023-12-04 11:52:50 +0100

    adde PHP dotenv

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInit731db5365d8cd92ebe7f50c0b4ac9002
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit731db5365d8cd92ebe7f50c0b4ac9002::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire731db5365d8cd92ebe7f50c0b4ac9002($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire731db5365d8cd92ebe7f50c0b4ac9002($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
