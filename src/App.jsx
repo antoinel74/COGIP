cdaf35794dc2a16ab192550caaa6f38e76a86186
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 10:43:52 +0100

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Home } from "./views/dashboard/home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
     </>
   );
