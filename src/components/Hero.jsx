3bee8e6eac03594766758ce8e1923725e9d5af66
antoinel74 <antoine.lansman@gmail.com>
2023-12-04 16:42:47 +0100

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = ({ text, img, yellowBg }) => {
 
   return (
     <div className={containerClass}>
-      <div className="w-1/2 p-4">
-        <h2 className="text-1xl md:text-4xl lg:text-6xl pl-12 font-bold">
+      <div className="w-1/2 p-2 md:p-4">
+        <h2 className="text-2xl md:text-4xl lg:text-6xl pl-12 font-bold">
           {text}
         </h2>
       </div>
-      <div className="w-1/2 p-4 bg-[url('/rectangle.png')] bg-contain bg-bottom bg-no-repeat">
+      <div className="w-1/2 p-2 md:p-4 bg-[url('/rectangle.png')] bg-contain bg-bottom bg-no-repeat">
         <img src={img} alt="vector-img" className="w-full h-auto" />
       </div>
     </div>
--- src/views/app/home.jsx
+++ src/views/app/home.jsx
@@ -3,14 +3,11 @@ import Hero from "../../components/Hero";
 import { Divider } from "../../components/Divider";
 import { Table } from "../../components/Table";
 
-
 const Home = () => {
   return (
     <div className="w-full">
       <Hero
-        text="MANAGE YOUR
-CUSTOMERS AND 
-INVOICES EASLY"
+        text="MANAGE YOUR CUSTOMERS AND INVOICES EASLY"
         img="/img/manager-1.png"
         yellowBg={true}
       />
@@ -19,9 +16,7 @@ INVOICES EASLY"
         <Table title="Last invoices" />
       </div>
       <Hero
-        text="WORK BETTER 
-IN YOUR
-COMPANY"
+        text="WORK BETTER IN YOUR COMPANY"
         img="/img/manager-2.png"
         yellowBg={false}
       />
