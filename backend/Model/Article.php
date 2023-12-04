8b8f8022c75f7b4bb1bfaf9d764d28923043f8e3
Venturi <bastien.aristide.venturi@gmail.com>
2023-12-04 13:37:06 +0100

    1 jour

---
+++ backend/Model/Article.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+class compagnies
+{
+    private $id_compagnies;
+    private $name;
+    private $type_id;
+    private $tva;
+    private $created_at;
+    private $updated_at;
+
+    public function __construct(int $id_compagnies, string $name, int $type_id, string $tva, string $created_at, string $updated_at)
+    {
+        $this->id_compagnies = $id_compagnies;
+        $this->name = $name;
+        $this->type_id = $type_id;
+        $this->tva = $tva;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    public function getId(): int
+    {
+        return $this->id_compagnies;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getTypeId(): int
+    {
+        return $this->type_id;
+    }
+
+    public function getTva(): string
+    {
+        return $this->tva;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+}
+
+
+
--- backend/index.php
+++ backend/index.php
@@ -4,3 +4,28 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/Core/Helper.php';
 require_once __DIR__.'/Routes/Routes.php';
 
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+//include all your model files here
+require 'Model/Article.php';
+//include all your controllers here
+require 'Controller/HomepageController.php';
+
+// Get the current page to load
+// If nothing is specified, it will remain empty (home should be loaded)
+$page = $_GET['page'] ?? null;
+
+// Load the controller
+// It will *control* the rest of the work to load the page
+switch ($page) {
+    case 'home':
+        (new HomeController())->index();
+        break;
+
+}
