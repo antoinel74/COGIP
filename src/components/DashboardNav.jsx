1aab4bc0fd4dcafa185f4578d387d3357014f017
antoinel74 <antoine.lansman@gmail.com>
2023-12-05 16:21:27 +0100

    add hero dashboard

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -8,7 +8,7 @@ const NavbarItem = ({ link, text, icon }) => {
         href={link}
       >
         <img className="h-5 w-5" src={icon} alt="icon" />
-        {text}
+        <span className="hidden md:block">{text}</span>
       </a>
     </li>
   );
@@ -36,20 +36,20 @@ export const DashboardNav = () => {
   ));
 
   return (
-    <div className="navbar-menu relative z-50">
-      <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-[250px] max-w-sm bg-white border-r overflow-y-auto">
-        <div className="flex flex-col items-center border-b border-gray-200 py-6 mb-8">
-          <img src="/.." alt="avatar" />
-          <h3 className="text-2xl font-semibold">Henry Georges</h3>
-        </div>
-        <ul>{renderedMenuItems}</ul>
-        <div className="mt-auto border-t border-gray-200 flex justify-between p-2">
-          <img src="/.." alt="avatar" />
-          <a href="/" className="text-[#9798d6]" alt="logout">
-            Log out
-          </a>
-        </div>
-      </nav>
-    </div>
+    <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-1/6 max-w-sm bg-white border-r overflow-y-auto">
+      <div className="flex flex-col items-center border-b border-gray-200 py-6 mb-8">
+        <img src="/.." alt="avatar" />
+        <h3 className="text-2xl font-semibold hidden md:block">
+          Henry Georges
+        </h3>
+      </div>
+      <ul>{renderedMenuItems}</ul>
+      <div className="mt-auto border-t border-gray-200 flex justify-between p-2">
+        <img src="/.." alt="avatar" className="hidden md:block" />
+        <a href="/" className="text-[#9798d6]" alt="logout">
+          Log out
+        </a>
+      </div>
+    </nav>
   );
 };
--- src/views/dashboard/DashboardHome.jsx
+++ src/views/dashboard/DashboardHome.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { DashboardNav } from "../../components/dashboardNav";
+import { DashboardHero } from "../../components/DashboardHero";
 
 export const DashboardHome = () => {
-  return <DashboardNav />;
+  return (
+    <div>
+      <DashboardNav />
+      <DashboardHero name="Henry" url="Dashboard/" />;
+    </div>
+  );
 };
