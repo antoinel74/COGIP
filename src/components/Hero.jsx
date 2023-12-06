25f6f4ce1f73a6a84085df17208f9ecba4f5544f
antoinel74 <antoine.lansman@gmail.com>
2023-12-06 12:19:51 +0100

    add GSAP animation on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Hero = ({ text, img, yellowBg }) => {
+  const textRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const textElement = textRef.current;
+    const imgElement = imgRef.current;
+
+    /* Animations */
+    if (textElement && imgElement) {
+      gsap.set([textElement], { opacity: 0, x: 40 });
+      gsap.set([imgElement], { opacity: 0, x: -40, scale: 0.9 });
+
+      const t1 = gsap.timeline({
+        defaults: { duration: 2, ease: "power2.out" },
+      });
+
+      /* Back to normal display */
+      t1.to([textElement, imgElement], {
+        opacity: 1,
+        stagger: { each: 0.07 },
+        x: 0,
+        scale: 1,
+      });
+    }
+  }, []);
+
   const containerClass = `flex justify-center items-center ${
     yellowBg ? "bg-[#f9de4e]" : "bg-transparent"
-  }`;
+  } hero-container`;
 
   return (
     <div className={containerClass}>
       <div className="w-1/2 p-2 md:p-4">
-        <h2 className="text-2xl md:text-4xl lg:text-6xl pl-12 font-bold">
+        <h2
+          className="text-2xl md:text-4xl lg:text-6xl pl-12 font-bold hero-text"
+          ref={textRef}
+        >
           {text}
         </h2>
       </div>
       <div className="w-1/2 p-2 md:p-4 bg-[url('/rectangle.png')] bg-contain bg-bottom bg-no-repeat">
-        <img src={img} alt="vector-img" className="w-full h-auto" />
+        <img
+          ref={imgRef}
+          src={img}
+          alt="vector-img"
+          className="w-full h-auto hero-img"
+        />
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style/index.css";
-import "./style/locomotive-scroll.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
