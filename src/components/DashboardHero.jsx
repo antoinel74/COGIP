72ca257b02ecacc3d3811eca16198c0b96210284
antoinel74 <antoine.lansman@gmail.com>
2023-12-07 10:04:13 +0100

    fix dashboard font-size

--- src/components/DashboardHero.jsx
+++ src/components/DashboardHero.jsx
@@ -5,12 +5,12 @@ export const DashboardHero = ({ name, url }) => {
     <div className="relative w-5/6 ml-auto right-0 mb-4">
       <div className="h-1/2 pb-8">
         <h1 className="text-3xl font-extrabold">Dashboard</h1>
-        <span>{url}</span>
+        <span className="text-sm">{url}</span>
       </div>
       <div className="h-1/2 bg-[#9798d6] rounded">
         <div className="w-full md:w-[50%] p-8 text-white">
           <h2 className="text-3xl font-semibold">Welcome back {name} !</h2>
-          <p className="font-light mt-2">
+          <p className="font-light mt-2 text-sm">
             You can here add an invoice, a company and some contacts
           </p>
         </div>
