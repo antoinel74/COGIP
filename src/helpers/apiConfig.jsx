13d9574d52b40d845a85946fd06370bc77275299
Pierre Marien <pierremarien29@gmail.com>
2023-12-08 12:06:56 +0100

    restructuration du dossier api

--- src/helpers/apiConfig.jsx
+++
@@ -1,8 +0,0 @@
-const baseURL = '';
-
-/**
- * path builder for api calls
- * @param {string} path the path to api's endpoint
- * @returns {buildApiUrl} the full path to api's endpoint
- */
-export const buildApiUrl = (path) => `${baseURL}${path}`;
