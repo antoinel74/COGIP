14e141d595cf5d5cd207c9b16a11c464a9dd31e7
antoinel74 <antoine.lansman@gmail.com>
2023-12-08 12:43:44 +0100

    populate list working for page 1

--- src/views/app/company.jsx
+++ src/views/app/company.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import jsonData from "../../assets/data.json";
 import { List } from "../../components/List";
 
 export const Company = () => {
-  return <List />;
+  const { companiesId } = useParams();
+  const [companyData, setCompanyData] = useState(null);
+
+  useEffect(() => {
+    console.log(jsonData);
+    const companies = jsonData.companies[0].data;
+    const company = companies.find(
+      (company) => company.id === parseInt(companiesId)
+    );
+    setCompanyData(company);
+  }, [companiesId]);
+
+  console.log(companyData);
+
+  return (
+    <div>
+      {companyData ? (
+        <List
+          name={companyData.name}
+          tva={companyData.tva}
+          country={companyData.country}
+          type={companyData.type}
+        />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
