95dbda6cbfa54a06ade0c4c17bf009010c3db008
Pierremarien <pierremarien29@gmail.com>
2023-12-11 15:54:48 +0100

    wip table encore et encore

--- src/components/Table2.jsx
+++ src/components/Table2.jsx
@@ -1,47 +1,102 @@
-import { useParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import { fetchPageByType } from '../helpers/api/FetchPageByType';
-import { fetchLast } from '../helpers/api/fetchLast';
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { fetchPageByType } from "../helpers/api/FetchPageByType";
+import { fetchLast } from "../helpers/api/fetchLast";
+import Searchbar  from "./Searchbar";
+import { getDataMap } from "../helpers/api/getDataMap";
+
+const getHeadersMap = (pageType) => {
+  if (pageType === "admin_panel") {
+    return {
+      companies: ["Name", "Tva", "Country"],
+      invoices: ["Invoice Number", "Dates", "Company"],
+      contacts: ["Name", "Phone", "Email"],
+    };
+  } else {
+    return {
+      companies: ["Name", "Tva", "Country", "Type", "Created At"],
+      invoices: ["Invoice number", "Due Dates", "Company", "Created At"],
+      contacts: ["Name", "Phone", "Mail", "Company", "Created At"],
+    };
+  }
+};
+
 
 /**
- * Table component taking a data type, a page number and a title as parameters	
+ * Table component taking a data type, a page number and a title as parameters
  * @param {string} dataType - Data parameter corresponding to "contacts", "invoices", "companies"
  * @param {number} pageNumber - Page number to fetch
  * @param {string} pageType - Page types parameters corresponding to "admin_panel", "application", "show"
- * @returns 
+ * @returns
  */
-export const Table2 = () => {
-  const { dataType } = useParams();
-  const [data, setData] = useState([]);
-  const [title, setTitle] = useState("");
-  const [pageType, setPageType] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
-  const [lastData, setLastData] = useState([]);
-  const [lastPage, setLastPage] = useState(1);
-  const [nextPage, setNextPage] = useState(2);
-  const [previousPage, setPreviousPage] = useState(0);
-  const [firstPage, setFirstPage] = useState(1);
+export const Table2 = ({ pageType, dataType }) => {
+  const headersMap = getHeadersMap(pageType);
+  const dataMap = getDataMap(pageType);
+  const headers = headersMap[dataType] || [];
+  const dataKeys = dataMap[dataType] || [];
+  // const { dataType } = useParams();
+  // const [data, setData] = useState([]);
+  // const [title, setTitle] = useState("");
+  // const [pageType, setPageType] = useState("");
+  // const [pageNumber, setPageNumber] = useState(1);
+  // const [lastData, setLastData] = useState([]);
 
-  useEffect(() => {
-    const fetchPage = async () => {
-      const page = await fetchPageByType(dataType, pageNumber);
-      setData(page);
-    };
+  // useEffect(() => {
+  //   const fetchPage = async () => {
+  //     const page = await fetchPageByType(dataType, pageNumber);
+  //     setData(page);
+  //   };
 
-    fetchPage();
-  }, [dataType, pageNumber]);
+  //   fetchPage();
+  // }, [dataType, pageNumber]);
 
-  useEffect(() => {
-    const fetchLastData = async () => {
-      const lastData = await fetchLast(dataType);
-      setLastData(lastData);
-    };
+  // useEffect(() => {
+  //   const fetchLastData = async () => {
+  //     const lastData = await fetchLast(dataType);
+  //     setLastData(lastData);
+  //   };
 
-    fetchLastData();
-  }, [dataType]);
-
+  //   fetchLastData();
+  // }, [dataType]);
  
-  return (
-    <div>Table2</div>
-  )
-}
+    return (
+      <div className={`overflow-x-auto mx-auto flex flex-col ${pageType === 'admin_panel' ? 'max-w-xl' : 'max-w-6xl'}`}>
+        {pageType === "application" || pageType === "show" && 
+            <div>
+              <h2 className="text-4xl w-full font-extrabold py-6 capitalize">{pageType === "show" ? "all" : "last"} {dataType}</h2>
+            </div>
+          }
+          {pageType ==="show" && <Searchbar />}
+        <table className="min-w-full table-fixed">
+          
+          <thead className={pageType === "application" || pageType === "show" ? "bg-[#f9de4e]" : ""}>
+            <tr>
+              {headers.map((header) => (
+                <th
+                  key={header}
+                  scope="col"
+                  className="py-3 px-6 text-xs font-bold tracking-wider text-left text-gray-900 uppercase"
+                >
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={item.id} className={(pageType === "application" || pageType === "show") ? (index % 2 === 1 ? "bg-gray-200" : "") : "" }>
+                {dataKeys.map((key) => (
+                  <td
+                    key={key}
+                    className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    {item[key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+};
---
+++ src/helpers/api/getDataMap.jsx
@@ -0,0 +1,22 @@
+/**
+ * Return the data map for the page
+ * @param {string} pageType     - Page types parameters corresponding to "admin_panel", "application", "show"
+ * @returns {object}            - Data map to use to display the different datas from the api 
+ */
+export const getDataMap = (pageType) => {
+    if (pageType === "admin_panel") {
+      return {
+        companies: ["name", "tva", "country"],
+        invoices: ["ref", "due_date", "company_name"],
+        contacts: ["name", "phone", "email"],
+      };
+    } else {
+      return {
+        companies: ["name", "tva", "country", "type", "created_at"],
+        invoices: ["ref", "due_date", "company_name", "created_at"],
+        contacts: ["name", "phone", "email", "company_name", "created_at"],
+      };
+    }
+  };
+
+   
