3c7ce070f3b9e0c3eb7518b55f205eb0cdfb5947
antoinel74 <antoine.lansman@gmail.com>
2023-12-19 13:33:59 +0100

    (fix css)

--- src/views/dashboard/DashboardHome.jsx
+++ src/views/dashboard/DashboardHome.jsx
@@ -6,7 +6,7 @@ export const DashboardHome = () => {
   return (
     <>
       <DashboardHero name="Henry" url="Dashboard/" />
-      <div className="flex flex-col lg:flex-nowrap lg:flex-row lg:gap-6  w-full">
+      <div className="flex flex-col lg:flex-nowrap lg:flex-row lg:gap-6 w-full">
         <div className="flex flex-col gap-6 w-full lg:w-1/2 overflow-hidden">
           <Card cardType="pile" />
           <Card cardType="table" dataType="companies" />
